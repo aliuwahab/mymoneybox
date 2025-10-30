be442542c0f0ad95fd63552a4380421800584e29
Aliu <aliuwahab@gmail.com>
2025-10-31 00:20:10 +0100

    add guest booking and also add the book a home option

--- database/migrations/2025_10_29_193905_add_country_id_to_users_table.php
+++ database/migrations/2025_10_29_193905_add_country_id_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('country_id')->nullable()->after('email')->constrained()->onDelete('set null');
-        });
+        if (!Schema::hasColumn('users', 'country_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->foreignId('country_id')->nullable()->after('email')->constrained()->onDelete('set null');
+            });
+        }
     }
 
     /**
