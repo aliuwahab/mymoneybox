477c850539f5612827724b91b1d0d80504fa4e77
Aliu <aliuwahab@gmail.com>
2026-01-03 12:59:11 +0100

    fix qr code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\AdminOnly;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,10 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => AdminOnly::class,
-        ]);
-
         // Exclude webhook routes from CSRF verification
         $middleware->validateCsrfTokens(except: [
             'webhooks/*',
