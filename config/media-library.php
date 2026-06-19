e136de75bd9c2c4ebe6c5b4b3e179ee31c3c9d37
Gbeila Aliu wahab <aliu.wahab@findock.com>
2026-06-19 12:32:45 +0000

    Fix media disk: default to public/local instead of s3

--- config/media-library.php
+++ config/media-library.php
@@ -6,13 +6,13 @@ return [
      * The disk on which to store added files and derived images by default. Choose
      * one or more of the disks you've configured in config/filesystems.php.
      */
-    'disk_name' => env('MEDIA_DISK', 's3'),
+    'disk_name' => env('MEDIA_DISK', 'public'),
 
     /*
      * The disk on which to store private/sensitive files (like KYC documents).
      * These files require temporary URLs for access (not public).
      */
-    'private_disk' => env('MEDIA_PRIVATE_DISK', 's3'),
+    'private_disk' => env('MEDIA_PRIVATE_DISK', 'local'),
 
     /*
      * The maximum file size of an item in bytes.
